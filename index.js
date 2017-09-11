cb8767fd56d359f0ce61c3d7c43b8bb10a6db80a
Kevin <kevin@Kevins-MBP.fios-router.home>
2017-09-10 20:12:49 -0400

    print output of shoe

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
 const cards = require('./lib/cards.js');
 const shuffler = require('./lib/shuffler.js');
+const Game = require('./lib/Game.js');
 
 let shuffledCards = shuffler(cards.casinoDeck);
 
+const game = new Game(shuffledCards);
+
+let i = 0;
+let histObj;
+
+while(game.dealRound() !== false){
+    console.log(`Game ${i}`);
+
+    histObj = game.history[i];
+
+    console.log(`Winner: ${histObj.winner}`);
+    console.log("Player hand: ");
+    console.log(histObj.playerHand.cards);
+    console.log(`Player total: ${histObj.playerHand.total}`);
+
+    console.log("Banker hand: ");
+    console.log(histObj.bankerHand.cards);
+    console.log(`Banker total: ${histObj.bankerHand.total}`);
+
+    console.log('=================================');
+    i++;
+
+};
+
+console.log(`Card penetration for this shoe: ${1-(game.stopAt/game.cards.length)}%`);
+
+process.exit();
